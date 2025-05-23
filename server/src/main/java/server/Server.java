90d8bf65fa0e88da47e7b0ca58fa40c0efb40be6
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-22 18:28:28 -0600

    Created first couple handlers for server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
 import io.javalin.Javalin;
+import model.response.result.ServiceException;
+import server.handler.ClearHandler;
+import server.handler.ExceptionHandler;
 
 public class Server {
 
@@ -10,7 +13,9 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        javalin.delete("/db", new ClearHandler());
 
+        javalin.exception(ServiceException.class, new ExceptionHandler());
     }
 
     public int run(int desiredPort) {
---
+++ server/src/main/java/server/handler/ExceptionHandler.java
@@ -0,0 +1,26 @@
+package server.handler;
+
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import model.response.ErrorResponse;
+import model.response.result.BadRequestException;
+import model.response.result.PreexistingException;
+import model.response.result.ServiceException;
+import model.response.result.UnauthorizedException;
+import org.jetbrains.annotations.NotNull;
+
+public class ExceptionHandler implements io.javalin.http.ExceptionHandler<ServiceException> {
+    @Override
+    public void handle(@NotNull ServiceException e, @NotNull Context context) {
+        context.status(getStatusCode(e)).json(new ErrorResponse(e.getMessage()));
+    }
+
+    private static HttpStatus getStatusCode(ServiceException e) {
+        return switch (e) {
+            case BadRequestException ignore -> HttpStatus.BAD_REQUEST;
+            case UnauthorizedException ignore -> HttpStatus.UNAUTHORIZED;
+            case PreexistingException ignore -> HttpStatus.FORBIDDEN;
+            default -> HttpStatus.INTERNAL_SERVER_ERROR;
+        };
+    }
+}

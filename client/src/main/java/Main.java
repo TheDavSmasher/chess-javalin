6c2bf63d0dac8ef80a4957885645e05b9d15d337
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-22 20:34:22 -0600

    Added most files from old implementation with new HttpClient

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,13 @@
-import chess.*;
+import chess.ChessGame;
+import chess.ChessPiece;
+import client.Repl;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        Repl repl = new Repl();
+        repl.run();
     }
 }
---
+++ client/src/main/java/client/HttpCommunicator.java
@@ -0,0 +1,58 @@
+package client;
+
+import model.response.ErrorResponse;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static model.Serializer.deserialize;
+
+public class HttpCommunicator {
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final HttpRequest.Builder requestBuilder = HttpRequest.newBuilder().timeout(Duration.ofMillis(5000));
+
+    public static <T> T doPost(String urlString, String body, String authToken, Class<T> responseClass) throws IOException {
+        return doServerMethod(urlString,  requestBuilder.POST(bodyPublisher(body)), authToken, responseClass);
+    }
+
+    public static <T> void doDelete(String urlString, String authToken, Class<T> responseClass) throws IOException {
+        doServerMethod(urlString, requestBuilder.DELETE(), authToken, responseClass);
+    }
+
+    public static <T> void doPut(String urlString, String body, String authToken, Class<T> responseClass) throws IOException {
+        doServerMethod(urlString, requestBuilder.PUT(bodyPublisher(body)), authToken, responseClass);
+    }
+
+    public static <T> T doGet(String urlString, String authToken, Class<T> responseClass) throws IOException {
+        return doServerMethod(urlString, requestBuilder.GET(), authToken, responseClass);
+    }
+
+    private static <T> T doServerMethod(String url, HttpRequest.Builder builder, String authToken, Class<T> responseClass) throws IOException {
+        builder.uri(URI.create(url));
+
+        if (authToken != null) {
+            builder.header("Authorization", authToken);
+        }
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(builder.build(), HttpResponse.BodyHandlers.ofString());
+        } catch (InterruptedException e) {
+            throw new IOException(e.getMessage());
+        }
+
+        if (response.statusCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException(deserialize(response.body(), ErrorResponse.class).message());
+        }
+        return deserialize(response.body(), responseClass);
+    }
+
+    private static HttpRequest.BodyPublisher bodyPublisher(String body) {
+        return HttpRequest.BodyPublishers.ofString(body);
+    }
+}

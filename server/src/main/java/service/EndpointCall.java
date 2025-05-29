f1923a6ae13336c1342b8d4485362eaed5b515d0
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-29 17:10:30 -0600

    Gave the FunctionalInterface annotation

--- server/src/main/java/service/EndpointCall.java
+++ server/src/main/java/service/EndpointCall.java
@@ -3,6 +3,7 @@ package service;
 import dataaccess.DataAccessException;
 import model.response.result.ServiceException;
 
+@FunctionalInterface
 public interface EndpointCall<T> {
     T method() throws ServiceException, DataAccessException;
 }

69fb6ed1dcb8ca1298f634e1c47408f5ff15e97a
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-06 15:27:17 -0600

    Removed unnecessary helper method

--- shared/src/main/java/model/response/UserEnterResponse.java
+++ shared/src/main/java/model/response/UserEnterResponse.java
@@ -1,9 +1,3 @@
 package model.response;
 
-import model.dataaccess.AuthData;
-
-public record UserEnterResponse(String username, String authToken) {
-    public static UserEnterResponse fromAuth(AuthData authData) {
-        return new UserEnterResponse(authData.username(), authData.authToken());
-    }
-}
+public record UserEnterResponse(String username, String authToken) {}

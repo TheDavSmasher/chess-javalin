fefce6de2b9b3ae26964f697959bcc7140e36c95
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-06 17:08:50 -0600

    Added VoidCall tryCatch

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -3,16 +3,19 @@ package service;
 import dataaccess.DataAccessException;
 
 public final class Service {
-    public interface EndpointCall<T> {
-        T method() throws ServiceException, DataAccessException;
-    }
-
     public static <T> T tryCatch(EndpointCall<T> call) throws ServiceException {
         try {
             return call.method();
         } catch (DataAccessException e) {
             throw new UnexpectedException(e.getMessage());
         }
+    }
+
+    public static <T> T tryCatch(VoidCall call) throws ServiceException {
+        return tryCatch(() -> {
+            call.method();
+            return null;
+        });
     }
 
     public static <T> T throwUnauthorized() throws UnauthorizedException {
@@ -23,11 +26,21 @@ public final class Service {
         throw new BadRequestException();
     }
 
-    public interface AuthorizedCall<T> {
-        T call(String username) throws ServiceException, DataAccessException;
-    }
-
     public static <T> T tryAuthorized(String authToken, AuthorizedCall<T> logic) throws ServiceException {
         return tryCatch(() -> logic.call(UserService.validateAuth(authToken)));
     }
+
+    //region Interfaces
+    public interface EndpointCall<T> {
+        T method() throws ServiceException, DataAccessException;
+    }
+
+    public interface VoidCall {
+        void method() throws ServiceException, DataAccessException;
+    }
+
+    public interface AuthorizedCall<T> {
+        T call(String username) throws ServiceException, DataAccessException;
+    }
+    //endregion
 }

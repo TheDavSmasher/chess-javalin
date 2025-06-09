289b47165db817efb77882240c029e5fd1e6eadb
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-09 13:24:16 -0600

    File reorder

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -3,16 +3,16 @@ package service;
 import dataaccess.DataAccessException;
 
 public abstract class Service {
+    protected interface EndpointSupplier<T> {
+        T method() throws ServiceException, DataAccessException;
+    }
+
     protected static <T> T tryCatch(EndpointSupplier<T> call) throws ServiceException {
         try {
             return call.method();
         } catch (DataAccessException e) {
             throw new UnexpectedException(e.getMessage());
         }
-    }
-
-    protected interface EndpointSupplier<T> {
-        T method() throws ServiceException, DataAccessException;
     }
 
     protected static <T> T throwUnauthorized() throws UnauthorizedException {

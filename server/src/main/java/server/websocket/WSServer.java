70f89ff7610947c7bed1d15cff0fd8d9c65da772
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-10 15:19:54 -0600

    Removed annotation and improved imports

--- server/src/main/java/server/websocket/WSServer.java
+++ server/src/main/java/server/websocket/WSServer.java
@@ -3,11 +3,7 @@ package server.websocket;
 import io.javalin.websocket.WsMessageContext;
 import io.javalin.websocket.WsMessageHandler;
 import org.eclipse.jetty.websocket.api.Session;
-import org.jetbrains.annotations.NotNull;
-import server.websocket.commands.WSConnect;
-import server.websocket.commands.WSLeave;
-import server.websocket.commands.WSMakeMove;
-import server.websocket.commands.WSResign;
+import server.websocket.commands.*;
 import websocket.commands.UserGameCommand;
 
 import java.io.IOException;
@@ -16,7 +12,7 @@ import static model.Serializer.serialize;
 
 public class WSServer implements WsMessageHandler {
     @Override
-    public void handleMessage(@NotNull WsMessageContext context) {
+    public void handleMessage(WsMessageContext context) {
         (switch (context.messageAsClass(UserGameCommand.class).getCommandType()) {
             case CONNECT -> new WSConnect();
             case MAKE_MOVE -> new WSMakeMove();

0e09547b4774a812b7d99a12c7613423c567c3c6
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-10 19:36:02 -0600

    Added Catcher overload

--- shared/src/main/java/model/Catcher.java
+++ shared/src/main/java/model/Catcher.java
@@ -1,13 +1,27 @@
 package model;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.function.Function;
 
 public final class Catcher {
     public interface ErrorSupplier<T> {
         T get() throws Exception;
     }
 
-    public static <T, R extends Exception> T catchRethrow(ErrorSupplier<T> supplier, Class<? extends Exception> catchClass, Class<R> rethrowClass) throws R {
+    public static <T, R extends Exception> T catchRethrow(
+            ErrorSupplier<T> supplier,
+            Class<? extends Exception> catchClass,
+            Class<R> rethrowClass
+    ) throws R {
+        return catchRethrow(supplier, catchClass, rethrowClass, Throwable::getMessage);
+    }
+
+    public static <T, R extends Exception> T catchRethrow(
+            ErrorSupplier<T> supplier,
+            Class<? extends Exception> catchClass,
+            Class<R> rethrowClass,
+            Function<Exception, String> errorMessage
+    ) throws R {
         try {
             return supplier.get();
         } catch (Exception e) {
@@ -16,7 +30,7 @@ public final class Catcher {
             }
             if (catchClass.isInstance(e)) {
                 try {
-                    throw rethrowClass.getConstructor(String.class).newInstance(e.getMessage());
+                    throw rethrowClass.getConstructor(String.class).newInstance(errorMessage.apply(e));
                 } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
                          InvocationTargetException ignored) {}
             }

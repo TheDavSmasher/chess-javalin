3be09be7b013e505d812ef8b13989f66df4fabed
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-11 14:35:09 -0600

    Reduced duplicate code

--- shared/src/main/java/utils/Catcher.java
+++ shared/src/main/java/utils/Catcher.java
@@ -41,25 +41,14 @@ public final class Catcher {
             Class<? extends R> throwAsClass,
             Function<Throwable, String> errorMessage
     ) throws R {
-        try {
-            return supplier.get();
-        } catch (Throwable e) {
-            if (RuntimeException.class.isAssignableFrom(e.getClass())) {
-                throw (RuntimeException) e;
-            }
-            if (rethrowClass.isAssignableFrom(e.getClass())) {
-                throw rethrowClass.cast(e);
-            }
-            if (catchClass != null && catchClass.isInstance(e)) {
-                try {
-                    throw throwAsClass.getConstructor(String.class, Throwable.class).newInstance(errorMessage.apply(e), e);
-                } catch (ReflectiveOperationException ignored) {}
-                try {
-                    throw throwAsClass.getConstructor(String.class).newInstance(errorMessage.apply(e));
-                } catch (ReflectiveOperationException ignored) {}
-            }
-            throw new RuntimeException(e);
-        }
+        return tryCatchInner(supplier, catchClass, rethrowClass, e -> {
+            try {
+                throw throwAsClass.getConstructor(String.class, Throwable.class).newInstance(errorMessage.apply(e), e);
+            } catch (ReflectiveOperationException ignored) {}
+            try {
+                throw throwAsClass.getConstructor(String.class).newInstance(errorMessage.apply(e));
+            } catch (ReflectiveOperationException ignored) {}
+        });
     }
 
     public interface ErrorRunnable {
@@ -96,16 +85,7 @@ public final class Catcher {
             Consumer<Throwable> postAction,
             Class<R> rethrowClass
     ) throws R {
-        try {
-            runnable.run();
-        } catch (Throwable e) {
-            if (rethrowClass != null && rethrowClass.isInstance(e)) {
-                throw rethrowClass.cast(e);
-            }
-            if (catchClass.isInstance(e)) {
-                postAction.accept(e);
-            }
-        }
+        tryCatchInner(() -> { runnable.run(); return null; }, catchClass, rethrowClass, postAction::accept);
     }
 
     public interface ErrorFunction<T, V> {
@@ -135,5 +115,31 @@ public final class Catcher {
             Function<Throwable, String> errorMessage
     ) throws R {
         return tryCatchResources(supplier, r -> r, function, catchClass, rethrowClass, errorMessage);
+    }
+
+    private interface ErrorConsumer<E extends Throwable> {
+        void accept(Throwable t) throws E;
+    }
+
+    private static <T, R extends Throwable> T tryCatchInner(
+            ErrorSupplier<T> supplier,
+            Class<? extends Throwable> catchClass,
+            Class<R> rethrowClass,
+            ErrorConsumer<R> onCatch
+    ) throws R {
+        try {
+            return supplier.get();
+        } catch (Throwable e) {
+            if (RuntimeException.class.isAssignableFrom(e.getClass())) {
+                throw (RuntimeException) e;
+            }
+            if (rethrowClass != null && rethrowClass.isInstance(e)) {
+                throw rethrowClass.cast(e);
+            }
+            if (catchClass != null && catchClass.isInstance(e)) {
+                onCatch.accept(e);
+            }
+            throw new RuntimeException(e);
+        }
     }
 }

0b67f92949ffe0a10bf949c19f4fa68c40f9dea5
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-06-17 16:59:22 -0600

    Created ClientCommandProcessing to hold interfaces and records used to refactor client

---
+++ client/src/main/java/client/states/ClientCommandProcessing.java
@@ -0,0 +1,28 @@
+package client.states;
+
+import client.ClientException;
+
+import java.io.IOException;
+
+public final class ClientCommandProcessing {
+    @FunctionalInterface
+    public interface CommandConsumer {
+        void process(String... params) throws ClientException, IOException;
+    }
+
+    @FunctionalInterface
+    public interface CommandRunnable {
+        void process() throws ClientException, IOException;
+    }
+
+    public record ClientCommand(
+            CommandConsumer command, String option, int paramMin, int paramMax, String format, String... description) {
+        public ClientCommand(CommandConsumer command, String option, int params, String format, String... description) {
+            this(command, option, params, params, format, description);
+        }
+
+        public ClientCommand(CommandRunnable command, String option, String... description) {
+            this(_ -> command.process(), option, 0, "This command takes no arguments", description);
+        }
+    }
+}

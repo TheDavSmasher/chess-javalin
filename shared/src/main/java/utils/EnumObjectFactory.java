7d30e676abcb3512b7f243a9f0d38d1fada55125
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-15 11:39:26 -0600

    Extracted generate to method

--- shared/src/main/java/utils/EnumObjectFactory.java
+++ shared/src/main/java/utils/EnumObjectFactory.java
@@ -3,12 +3,19 @@ package utils;
 import java.util.EnumMap;
 
 public abstract class EnumObjectFactory<K extends Enum<K>, V> {
+    private boolean mapGenerated = false;
     private final EnumMap<K, V> preMadeObjects = new EnumMap<>(getKeyClass());
 
     protected EnumObjectFactory() {
+        generateValues();
+    }
+
+    protected void generateValues() {
+        if (mapGenerated) return;
         for (K key : getKeyClass().getEnumConstants()) {
             preMadeObjects.put(key, preGenerateValue(key));
         }
+        mapGenerated = true;
     }
 
     public V get(K key) {

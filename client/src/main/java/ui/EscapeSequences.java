ca0750d8cd14eb5982bf866ecffb06c0b13adca3
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-29 19:31:36 -0600

    Removed chess piece strings for helper methods

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -1,5 +1,8 @@
 package ui;
 
+import chess.ChessGame;
+import chess.ChessPiece;
+
 import java.util.function.Function;
 
 /**
@@ -90,18 +93,14 @@ public final class EscapeSequences {
     //endregion
 
     //region Pieces
-    public static final String WHITE_KING = " ♔ ";
-    public static final String WHITE_QUEEN = " ♕ ";
-    public static final String WHITE_BISHOP = " ♗ ";
-    public static final String WHITE_KNIGHT = " ♘ ";
-    public static final String WHITE_ROOK = " ♖ ";
-    public static final String WHITE_PAWN = " ♙ ";
-    public static final String BLACK_KING = " ♚ ";
-    public static final String BLACK_QUEEN = " ♛ ";
-    public static final String BLACK_BISHOP = " ♝ ";
-    public static final String BLACK_KNIGHT = " ♞ ";
-    public static final String BLACK_ROOK = " ♜ ";
-    public static final String BLACK_PAWN = " ♟ ";
+    public static String getAsUnicode(ChessPiece piece) {
+        return getAsUnicode(piece.type(), piece.getTeamColor() == ChessGame.TeamColor.WHITE);
+    }
+
+    public static String getAsUnicode(ChessPiece.PieceType type, boolean isWhite) {
+        return " " + ('♔' + type.ordinal() + (isWhite ? 0 : 6)) + " ";
+    }
+
     public static final String EMPTY = " \u2003 ";
     //endregion
 

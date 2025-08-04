1864a626cac4667ab792c6a5ed5b872821f06295
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-08-04 16:21:33 -0600

    Deleted ChessClient after combining state management to StateManager

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,26 +1,29 @@
 package client;
 
+import client.states.ClientStateManager;
 import ui.EscapeSequences.Format;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static utils.Catcher.*;
 
 public class Repl {
     private final PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-    private final ChessClient client = new ChessClient(out);
+    private final ClientStateManager clientStates = new ClientStateManager(out);
+    private final Scanner scanner = new Scanner(System.in);
 
     public void run() {
         out.println("Welcome to my Chess Server!\nRun the 'help' command to see available commands.");
 
-        Scanner scanner = new Scanner(System.in);
         while (true) {
             printPrompt(out);
 
             tryCatchDo(() -> tryCatchDo(
-                () -> client.evaluate(scanner.nextLine()),
+                () -> evaluate(scanner.nextLine()),
             ExitException.class, _ -> {
                 out.println();
                 System.exit(0);
@@ -28,6 +31,19 @@ public class Repl {
                 e -> out.print(e.getMessage())
             );
         }
+    }
+
+    private void evaluate(String input) throws ClientException {
+        String[] tokens = input.toLowerCase().split(" ");
+        tryCatchDo(() -> tryCatchRethrow(() -> {
+            clientStates.getCurrentState().evaluate(
+                    (tokens.length > 0 ? Integer.parseInt(tokens[0]) : 0) - 1,
+                    Arrays.copyOfRange(tokens, 1, tokens.length));
+            return null;
+        }, IOException.class, ClientException.class),
+        NumberFormatException.class,
+        _ -> clientStates.getCurrentState().help(true),
+        ClientException.class);
     }
 
     private void printPrompt(PrintStream out) {

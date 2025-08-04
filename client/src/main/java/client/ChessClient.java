47f93e38ef63dcbbcd395cc94c00a5cfba587560
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-08-04 16:01:01 -0600

    Extracted ClientState EnumMap to own factory class

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -6,50 +6,33 @@ import client.states.ClientStateManager.MenuState;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
-import utils.EnumObjectFactory;
 
 import static utils.Catcher.*;
 
-public class ChessClient extends EnumObjectFactory<MenuState, ChessClientState> {
-    private final ClientStateManager clientStateManager;
+public class ChessClient {
+    private final ClientStateFactory clientStates;
 
     private MenuState currentState = null;
 
     public ChessClient(PrintStream out) {
-        super(false);
-        clientStateManager = new ClientStateManager(this, out);
-        generateValues();
-    }
-
-    @Override
-    protected Class<MenuState> getKeyClass() {
-        return MenuState.class;
-    }
-
-    @Override
-    protected ChessClientState generateValue(MenuState key) {
-        return switch (key) {
-            case PRE_LOGIN -> new PreLoginClientState(clientStateManager);
-            case POST_LOGIN -> new PostLoginClientState(clientStateManager);
-            case MID_GAME -> new InGameClientState(clientStateManager);
-        };
+        clientStates = new ClientStateFactory(new ClientStateManager(this, out));
     }
 
     public void evaluate(String input) throws ClientException {
         String[] tokens = input.toLowerCase().split(" ");
         tryCatchDo(() -> tryCatchRethrow(() -> {
-            get(currentState).evaluate(
+            clientStates.get(currentState).evaluate(
                     (tokens.length > 0 ? Integer.parseInt(tokens[0]) : 0) - 1,
                     Arrays.copyOfRange(tokens, 1, tokens.length));
             return null;
         }, IOException.class, ClientException.class),
         NumberFormatException.class,
-            _ -> get(currentState).help(true),
+            _ -> clientStates.get(currentState).help(true),
         ClientException.class);
     }
 
     public void changeState(MenuState state) {
         currentState = state;
-        get(currentState).help(false);
+        clientStates.get(currentState).help(false);
     }
 }

b0bea6e118ef22efe69734df20c85e7742af9f43
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-10-31 13:44:50 -0600

    Modified tests to match removed method

--- server/src/test/java/dataaccess/SQLUserDAOTest.java
+++ server/src/test/java/dataaccess/SQLUserDAOTest.java
@@ -25,22 +25,16 @@ class SQLUserDAOTest {
         userDAO.createUser(username, password, email);
 
         UserData normal = userDAO.getUser(username);
-        UserData withPassword = userDAO.getUser(username, password);
 
         Assertions.assertNotNull(normal);
-        Assertions.assertNotNull(withPassword);
         Assertions.assertTrue(BCrypt.checkpw(password, normal.password()));
-        Assertions.assertTrue(BCrypt.checkpw(password, withPassword.password()));
     }
 
     @Test
     void getUserFail() throws DataAccessException {
         Assertions.assertNull(userDAO.getUser(username));
-        Assertions.assertNull(userDAO.getUser(username, password));
         userDAO.createUser(username, password, email);
         Assertions.assertNull(userDAO.getUser("nonexistent"));
-        Assertions.assertNull(userDAO.getUser(username, "wrong-password"));
-        Assertions.assertNull(userDAO.getUser("nonexistent", password));
     }
 
     @Test

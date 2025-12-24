0a67dc8ce006b24b641714725c833f84c33e0ead
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-12-23 20:18:03 -0700

    Added non-check rules for Castling

--- shared/src/main/java/chess/calculator/KingMoveCalculator.java
+++ shared/src/main/java/chess/calculator/KingMoveCalculator.java
@@ -1,5 +1,10 @@
 package chess.calculator;
 
+import chess.*;
+import utils.BooleanCombinations;
+
+import java.util.Collection;
+
 public class KingMoveCalculator extends SymmetricCalculator {
     private static final IntTuple.Modifier[] modifiers = {
             IntTuple::invert, IntTuple::flatten, IntTuple::rotate
@@ -13,5 +18,43 @@ public class KingMoveCalculator extends SymmetricCalculator {
     @Override
     protected boolean isContinuous() {
         return false;
+    }
+
+    @Override
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition start) {
+        Collection<ChessMove> moves = super.calculateMoves(board, start);
+
+        ChessPiece king = board.getPiece(start);
+        int teamRow = king.getTeamColor().initialRow();
+        if (king.hasMoved() || start.getRow() != teamRow) {
+            return moves;
+        }
+
+        for (var isRight : new BooleanCombinations(1).next().values()) {
+            int dirMod = isRight ? 1 : -1;
+            ChessPosition endPosition = new ChessPosition(teamRow, start.getRow() + dirMod * 2);
+            if (endPosition.outOfBounds()) {
+                continue;
+            }
+
+            int rookCol = isRight ? 8 : 1;
+            ChessPosition rookPos = new ChessPosition(teamRow, rookCol);
+            if (!(board.getPiece(rookPos) instanceof ChessPiece rook) || rook.hasMoved()) {
+                continue;
+            }
+
+            boolean cleanPath = true;
+            for (int c = start.getColumn() + dirMod; c < rookCol; c += dirMod) {
+                if (board.getPiece(new ChessPosition(teamRow, c)) != null) {
+                    cleanPath = false;
+                    break;
+                }
+            }
+            if (cleanPath) {
+                moves.add(new ChessMove(start, endPosition));
+            }
+        }
+
+        return moves;
     }
 }
